b70f087b22cb2083df9647794ae232801520ca01
ZINKTiti <zinktiti@hotmail.fr>
2017-12-11 23:48:48 +0100

    V0.4
    New code

--- Angle_and_tow_meter/Angle_and_tow_meter.ino
+++ Angle_and_tow_meter/Angle_and_tow_meter.ino
@@ -15,27 +15,37 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
-float xg,yg,zg;
-float soh;
-float tilt;
-float angle;
-String dir;
-
 const int PB_PLUS = 2;
 const int PB_MINUS = 3;
 const int PB_INIT = 4;
 
-int CHORD = 50;
-float DISPLAYANGLE, DISTANCE, RADIAN, STARTANGLE;
+// Variables 
+int corde_gouverne = 50;     // Corde de la gouverne
+
+//  Vecteurs acceleration (3 axe)
+//              x,y,z
+double v_neutre [3] = {0,0,0};
+double v_courant [3] = {0,0,0};
+// Variables de calcul
+double produit_v;
+double norme_v_neutre;
+double norme_v_courant;
+float angle;
+float debattement;
+// Variable de gestion de l'affichage
+char ligne_texte [17];
+int angle_aff;
+int debattement_aff;
+//debug
+boolean debug = false;
 
 void setup(void) 
 {
   Serial.begin(9600);
-  Serial.println("Accelerometer Test"); Serial.println("");
 
-  pinMode(PB_PLUS,INPUT);
-  pinMode(PB_MINUS, INPUT);
-  pinMode(PB_INIT, INPUT);
+//  pinMode(PB_PLUS,INPUT);
+//  pinMode(PB_MINUS, INPUT);
+//  pinMode(PB_INIT, INPUT);
   
   /* Initialise the sensor */
   if(!accel.begin())
@@ -52,13 +62,6 @@ void setup(void)
   // displaySetRange(ADXL345_RANGE_2_G);
   //accel.setDataRate(ADXL345_DATARATE_3200_HZ);
   
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C
   display.clearDisplay();   // clears the screen and buffer   // Efface l'écran
@@ -70,159 +73,86 @@ void loop(void)
   /* Get a new sensor event */ 
   sensors_event_t event; 
   accel.getEvent(&event);
- 
-  /* Display the results (acceleration is measured in m/s^2) */
-  //Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  //Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  //Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
-  delay(500);
-  xg = event.acceleration.x*0.0039;
-  yg = event.acceleration.y*0.0039;
-  zg = event.acceleration.z*0.0039;
-  // Output x,y,z values - Commented out
-  //Serial.print("X Value: "); Serial.print(xg);
-  //Serial.print(", Y Value: "); Serial.print(yg);
-  //Serial.print(", Z Value: "); Serial.println(zg);
-  delay(500);
-
+   
+  // Lecture du capteur LIS3DH
+  v_courant [0] = event.acceleration.z;
+  v_courant [1] = event.acceleration.y;
+  v_courant [2] = event.acceleration.z;
   
-  //soh = yg/zg;
-  angle = abs(atan(yg/zg)*57.296);
-  DISPLAYANGLE = abs(angle-STARTANGLE);
-  RADIAN = (abs(angle-STARTANGLE)* 71) / 4068;
-  DISTANCE = sin(RADIAN) * CHORD;
+  // plan de calcul : y/z :
+  // axes : x:0  y:1  z:2
+  // Calcul du produit : v_neutre . v_courant 
+    produit_v = v_neutre [1] * v_courant [1] + v_neutre [2] * v_courant [2];
+  //
+  // Calcul norme : v_neutre 
+    norme_v_neutre = sqrt (sq (v_neutre [1]) + sq (v_neutre [2]));
+  //
+  // Calcul norme : v_courant 
+    norme_v_courant = sqrt (sq (v_courant [1]) + sq (v_courant [2]));
+  //
+  // Calcul de l'angle en radians
+    angle = acos (produit_v  / (norme_v_neutre * norme_v_courant));
+  //  debattement gouverne
+  //  Equation = 2 x sinus (Angle/2) * corde gouverne
+    debattement = abs( (corde_gouverne * 2.0) * sin (angle/2.0));
+  // Conversion de l'angle en degres
+    angle = abs(angle * 180 / 3.14159265359);
+
+
   
   if (digitalRead(PB_INIT) == LOW)
   {
-    STARTANGLE = angle;
+     v_neutre [1] = event.acceleration.y;
+     v_neutre [2] = event.acceleration.z;
+     delay(100);
   }
   
   if (digitalRead(PB_PLUS) == LOW)
   {
-    CHORD++;
+    corde_gouverne++;
+    delay(100);
   }
   if (digitalRead(PB_MINUS) == LOW) 
   {
-    CHORD--; 
+    corde_gouverne--;
+    delay(100); 
   }
-  
-  Serial.print("Tilt Angle is: "); Serial.print(angle); Serial.print(";");
-  Serial.print(" Display Angle is: "); Serial.print(DISPLAYANGLE); Serial.print(";");
-  Serial.print(" Start angle :"); Serial.print(STARTANGLE);Serial.print(";");
-  Serial.print(" Chord :"); Serial.print(CHORD);Serial.print(";");
-  Serial.print(" Radian :"); Serial.print(RADIAN); Serial.print(";");
-  Serial.print(" Distance :");Serial.print(DISTANCE); Serial.println(";");
+
+  angle_aff = (int) (angle*10);
+  debattement_aff = (int) (debattement*10);
+ 
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.print("CHORD "); display.println(CHORD);
-  display.print("ANGLE "); display.print(DISPLAYANGLE); display.println(" deg");
-  display.print("DIST "); display.print(DISTANCE); display.println(" mm"); 
+  display.print("Chord "); display.println(corde_gouverne);
+  display.print("ANGLE "); display.println(angle_aff/10);/*display.print(",");display.print(angle_aff%10); display.println(" deg")*/;
+  //display.print("Deb "); 
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(80,0);
+  display.print(debattement_aff/10);/*display.print(",");display.print(debattement_aff%10);display.println(" mm")*/; 
   display.display();
   delay(5);
   display.clearDisplay();
-}
+  delay(75);
 
 
-
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-  
-  switch(accel.getDataRate())
+  if (debug == true)
   {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 "); 
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 "); 
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 "); 
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 "); 
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 "); 
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 "); 
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 "); 
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 "); 
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 "); 
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 "); 
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 "); 
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 "); 
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 "); 
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 "); 
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 "); 
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 "); 
-      break;
-    default:
-      Serial.print  ("???? "); 
-      break;
-  }  
-  Serial.println(" Hz");  
+    Serial.print(" v_courant_x: "); Serial.print(v_courant [0]); Serial.print(";");
+    Serial.print(" v_courant_y: "); Serial.print(v_courant [1]); Serial.print(";");
+    Serial.print(" v_courant_z: "); Serial.print(v_courant [2]); Serial.print(";");
+    Serial.print(" v_neutre_x: "); Serial.print(v_neutre [0]); Serial.print(";");
+    Serial.print(" v_neutre_y: "); Serial.print(v_neutre [1]); Serial.print(";");
+    Serial.print(" v_neutre_z: "); Serial.print(v_neutre [2]); Serial.print(";");
+    Serial.print(" produit_v: "); Serial.print(produit_v); Serial.print(";");
+    Serial.print(" norme_v_neutre: "); Serial.print(norme_v_neutre); Serial.print(";");
+    Serial.print(" norme_v_courant: "); Serial.print(norme_v_courant); Serial.print(";");
+    Serial.print(" Angle: "); Serial.print(angle); Serial.print(";");
+    Serial.print(" Angle_aff: ");Serial.print(angle_aff/10);Serial.print(",");Serial.print(angle_aff%10); Serial.print(" deg"); Serial.print(";");
+    Serial.print(" Chord :"); Serial.print(corde_gouverne);Serial.print(";");
+    Serial.print(" Debatement :");Serial.print(debattement_aff/10);Serial.print(",");Serial.print(debattement_aff%10); Serial.println(";");
+  }
+  
 }
 
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-  
-  switch(accel.getRange())
-  {
-    case ADXL345_RANGE_16_G:
-      Serial.print  ("16 "); 
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print  ("8 "); 
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print  ("4 "); 
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print  ("2 "); 
-      break;
-    default:
-      Serial.print  ("?? "); 
-      break;
-  }  
-  Serial.println(" g");  
-}
